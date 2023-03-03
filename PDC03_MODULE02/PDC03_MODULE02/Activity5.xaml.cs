746444c2b8d9288e8ef7e954adc702a962341a9b
Clavory <allapitan.nathaniel@auf.edu.ph>
2023-03-03 10:11:59 +0800

    Final Commit

---
+++ PDC03_MODULE02/PDC03_MODULE02/Activity5.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.ObjectModel;
+
+namespace PDC03_MODULE02
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Activity5 : ContentPage
+    {
+        ObservableCollection<employee> employees = new ObservableCollection<employee>();
+        public ObservableCollection<employee> Employees { get { return employees; } }
+
+        public Activity5()
+        {
+            InitializeComponent();
+
+            {
+                lst.ItemsSource = employees;
+
+                employees.Add(new employee { DisplayName = "Nathaniel Allapitan", Position = "President" ,profileImage="image3.jpg" });
+                employees.Add(new employee { DisplayName = "Clavory", Position = "Vice President", profileImage = "image2.jpg" });
+                employees.Add(new employee { DisplayName = "Syncophantam", Position = "Secretary" , profileImage = "image1.jpg" });
+             
+            }
+        }
+    }
+}
